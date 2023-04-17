7a61ebdb1ec3fb40d005cbadfcddf5dffbf2fd7c
Jhonata Oliveira <oliveira720@outlook.com.br>
2023-04-17 04:19:30 -0300

    first commit

---
+++ src/Rodape/rodape.jsx
@@ -0,0 +1,38 @@
+import React from "react"; 
+
+
+function rodape(){      
+    
+  return(
+    
+         <footer>
+            <h6 class="n1">
+                Jhonata Oliveira
+            </h6>
+            <p>Sigame em mihas redes sociais</p>
+
+            <div class="Redes-sociais">
+            <a href="https://www.facebook.com/" target="_blank">Facebook</a>
+          <a href="https://www.instagram.com/jhonata_oli02/" target="_blank">Instagram</a>
+          <a href="https://twitter.com/Jhonata03017990" target="_blank">twitter</a>
+                 </div>
+        <div class="End">
+        <p>&copy;ByJhonata</p>
+                </div>
+
+        <div id="btnTop">
+          <i class="arrow up"></i>
+                </div>
+         </footer>
+         
+         
+         
+  
+        
+    )
+
+
+
+}
+
+export default rodape;
